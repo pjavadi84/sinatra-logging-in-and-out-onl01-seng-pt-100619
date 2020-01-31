20d176cf87f678e9687a84e994cf7c9871c545ce
Seyedpouya hajiseyedjavadi <pouya.javadi.resume@gmail.com>
2020-01-31 19:27:19 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,13 @@
 require_relative '../../config/environment'
 class ApplicationController < Sinatra::Base
+  
+  
   configure do
     set :views, Proc.new { File.join(root, "../views/") }
     enable :sessions unless test?
     set :session_secret, "secret"
   end
+  
 
   get '/' do
     erb :index
@@ -14,19 +17,23 @@ class ApplicationController < Sinatra::Base
     @user = User.find_by(username: params[:username])
     
     if @user
+      # line 18, is where the session is being created if the @user that comes from our helpers.rb, exists.
       session[:user_id] = @user.id
       redirect to '/account'
       erb :account
+    else
+      erb :error 
     end
-    
+  
   end
 
   get '/account' do
-
+    erb :account
   end
 
   get '/logout' do
-
+    session.clear
+    redirect to '/'
   end
 
 
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,5 +1,15 @@
 class Helpers
   def self.current_user(session)
     @user = User.find_by(id: session[:user_id])
+    # binding.pry
   end
+  
+  def self.is_logged_in?(session)
+    if @user
+      !!session[:user_id]
+    else
+      !session[:user_id]
+    end
+  end
+
 end
